f463d0d301729ac29d22b24b15c03d49617cb48d
drahmetacik <a.acik@icloud.com>
2025-10-12 19:58:13 +0100

    Remove unused import of LocalDate in JdbcBatchProcessingTest

--- src/test/java/databaseTests/JdbcBatchProcessingTest.java
+++ src/test/java/databaseTests/JdbcBatchProcessingTest.java
@@ -9,7 +9,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
